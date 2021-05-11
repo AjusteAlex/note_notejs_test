c6f0df3733f2c0929c5c9af242472c9c1019bbf7
Chralu <contact@charly-caulet.net>
2021-05-11 10:20:35 +0200

    Refacto : Extraction des routes.
    Co-authored-by: paul-andre-leveque <paul-andre-leveque@users.noreply.github.com>
    Co-authored-by: aymone2k <aymone2k@users.noreply.github.com>
    Co-authored-by: Mario Chacon <mariochacondev@users.noreply.github.com>
    Co-authored-by: Jerome <jtridat@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,43 +1,12 @@
 require('dotenv').config()
+require('./persistence/db').config()
 
 const express = require('express')
 const app = express()
 
-
-const mongoose = require('mongoose')
 app.use(express.json())
 
-const { Note } = require('./model/note')
-
-require('./persistence/db').config()
-
-app.get('/note/:note_id', async (req, res) => {
-
-    const note = await Note.findById(req.params.note_id)
-    res.send(note)
-})
-
-app.post('/note', async (req, res) => {
-    try {
-        const currentNote = new Note({
-            title: req.body.title,
-            content: req.body.content
-        })
-
-        const createdNote = await currentNote.save();
-
-        res.setHeader('Location', `/note/${createdNote.id}`)
-        res.status(201)
-        res.send(createdNote)
-    } catch (exception) {
-        if (exception instanceof mongoose.Error.ValidationError) {
-            res.status(400).json({ message: exception.message })
-            return
-        }
-
-        res.sendStatus(500)
-        console.error(exception)
-    }
-})
+const noteRouter = require('./route/note')
+app.use('/', noteRouter)
 
 app.listen(process.env.PORT)
